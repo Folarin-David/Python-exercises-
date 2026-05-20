31fd39d06ed9288b5f5d338d226ffe579728119d
Folarin-David <emmanuelfolarin2803@gmail.com>
2026-05-20 01:04:19 -0700

    uploading  my python exercise update to github

---
+++ chapter 03/exercise 1.py
@@ -0,0 +1,16 @@
+#Exercise 1
+# Asking for hours worked and convert the input to a float for decimal support
+hours = float(input("Enter hours worked: "))
+
+# Asking for rate per hour and convert the input to a float
+rate = float(input("Enter rate per hour: "))
+
+# Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+if hours > 40:
+    rate = 1.5 * rate
+    
+# Calculating gross pay by multiplying hours and rate
+gross_pay = hours * rate
+
+# Displaying the result
+print("Gross pay:", gross_pay)
